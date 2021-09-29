f797144f9511c0a75347b6b271d1775da978603d
Beleiu Cristina Nicoleta <61590793+Nico-git85@users.noreply.github.com>
2021-09-29 21:40:56 +0300

    geometries/bufferAttribute/positions

--- src/script.js
+++ src/script.js
@@ -11,49 +11,52 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
-/**
- * Object
- */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0x28FFBF })
+// Object
+// const geometry = new THREE.BoxBufferGeometry(1, 1, 1, 4, 4, 4)
+const geometry = new THREE.BufferGeometry()
+
+const count = 500
+const positionsArray = new Float32Array(count * 3 * 3)
+
+for(let i = 0; i < count *3 *3; i++){
+    positionsArray[i] =( Math.random() -0.5) * 5
+}
+
+
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3)
+geometry.setAttribute('position', positionsAttribute  )
+
+
+
+const material = new THREE.MeshBasicMaterial({ 
+    color: 0xFDB9FC,
+    wireframe: true 
+})
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
-/**
- * Sizes
- */
+// Sizes
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
 }
 
-window.addEventListener("resize", () => {
-    //Update sizes
+window.addEventListener('resize', () =>
+{
+    // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
 
-    //Update Camera
-  camera.aspect = sizes.width / sizes.height
-  camera.updateProjectionMatrix()
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
 
-  //Update renderer
-  renderer.setSize(sizes.width, sizes.height)  
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
-window.addEventListener("dblclick", () => {
-   if(!document.fullscreenElement){
-       canvas.requestFullscreen()
-   }else {
-      document.exitFullscreen()
-   }
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
-
-
-/**
- * Camera
- */
-// Base camera
+// Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.z = 3
 scene.add(camera)
@@ -62,18 +65,14 @@ scene.add(camera)
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
-/**
- * Renderer
- */
+// Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-
-/**
- * Animate
- */
+// Animate
 const clock = new THREE.Clock()
 
 const tick = () =>
