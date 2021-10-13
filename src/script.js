972ee35012555d591434d4f9cad12d32864fdeed
Beleiu Cristina Nicoleta <61590793+Nico-git85@users.noreply.github.com>
2021-10-14 00:04:41 +0300

    Texture

--- src/script.js
+++ src/script.js
@@ -1,22 +1,59 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import gsap from 'gsap'
-import * as dat from 'dat.gui'
+
 
 /**
- * Debug
+ * Textures
  */
-const gui =new dat.GUI({closed: true, width: 400 })
+const loadingManager = new THREE.LoadingManager()
+// loadingManager.onStart = () => {
+//     console.log('onStart');
+// }
+
+// loadingManager.onLoad = () => {
+//     console.log('onLoad');
+// } 
+
+// loadingManager.onProgress = () => {
+//     console.log('onProgress');
+// }
+
+// loadingManager.onError = () => {
+//     console.log('onError');
+// }
+
+
+const textureLoader = new THREE.TextureLoader(loadingManager)
+const colorTexture = textureLoader.load('/textures/minecraft.png')
+const alphaTexture = textureLoader.load('/textures/door/alpha.jpg')
+const heightTexture = textureLoader.load('/textures/door/height.jpg')
+const normalTexture = textureLoader.load('/textures/door/normal.jpg')
+const ambientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
+const metalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
+const roughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
+
+
+// colorTexture.repeat.x = 2
+// colorTexture.repeat.y = 3
+// colorTexture.wrapS = THREE.MirroredRepeatWrapping
+// colorTexture.wrapT = THREE.MirroredRepeatWrapping
+
+
+// colorTexture.offset.x = 0.5
+// colorTexture.offset.y = 0.5
+
+// colorTexture.rotation = Math.PI * 0.25
+// colorTexture.center.x = 0.5
+// colorTexture.center.y = 0.5
+
+colorTexture.generateMipmaps = false
+colorTexture.minFilter = THREE.NearestFilter
+colorTexture.magFilter = THREE.NearestFilter
 
 
 
-const parameters = {
-    color: 0xF21170,
-    spin: () =>{
-       gsap.to(mesh.rotation, {duration:1, y: mesh.rotation.y +  10})
-    }
-}
+
 
 
 /**
@@ -32,18 +69,10 @@ const scene = new THREE.Scene()
  * Object
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: parameters.color })
+const material = new THREE.MeshBasicMaterial({ map: colorTexture })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
-//Debug
-gui.add(mesh.position, 'y').min(-3).max(3).step(0.01).name('elevation')
-gui.add(mesh, 'visible')
-gui.add(material, 'wireframe')
-gui.addColor(parameters, 'color').onChange(() => {
-     material.color.set(parameters.color)
-})
-gui.add(parameters, 'spin')
 /**
  * Sizes
  */
@@ -72,7 +101,9 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.z = 3
+camera.position.x = 1
+camera.position.y = 1
+camera.position.z = 1
 scene.add(camera)
 
 // Controls
